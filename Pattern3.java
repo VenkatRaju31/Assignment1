67305806fed55f76e9dcfe7a1e839868d195c081
Venkat <naveenraju138@gmail.com>
2022-10-26 16:57:48 +0530

    commit all prgms

---
+++ Pattern3.java
@@ -0,0 +1,23 @@
+package NestedLooping;
+
+public class Pattern3 {
+	public static void main(String[] args) {
+		int n=14;
+		for (int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i+j<(n)/2||j==0||i==0||j==n-1||i==n-1||j-i>=n/2)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
